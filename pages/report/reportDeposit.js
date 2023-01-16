dd7f11c246dee7a9150822e35b87ea45d404f830
khemwichayut <54529388+khemwichayut@users.noreply.github.com>
2023-01-16 14:08:08 +0700

    bank

---
+++ pages/report/reportDeposit.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../theme/Layout'
+
+function reportDeposit() {
+  return (
+    <Layout>
+      <div>
+      reportDeposit
+    </div>  
+    </Layout>
+    
+  )
+}
+
+export default reportDeposit
---
+++ pages/report/reportMakeDeposit.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../theme/Layout'
+
+function reportMakeDeposit() {
+    return (
+        <Layout>
+            <div>
+                reportMakeDeposit
+            </div>
+        </Layout>
+
+    )
+}
+
+export default reportMakeDeposit
