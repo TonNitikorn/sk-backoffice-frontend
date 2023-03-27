0a2240fe6ed142ed326ab7ff2618c347b9798706
khemwichayut <54529388+khemwichayut@users.noreply.github.com>
2023-03-27 17:33:50 +0700

    host

--- utils/hostname.js
+++ utils/hostname.js
@@ -1,5 +1,5 @@
 // const hostname = "https://api-007bet.superfast-auto.com";
 // const hostname = "http://localhost:5001";
-const hostname = "https://bo-api.angpaos.cloud/"
+const hostname = "https://bo-api.angpaos.cloud"
 
 export default hostname;
