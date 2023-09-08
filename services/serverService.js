99191df33cd46051b9c74c83d7ef1cc8fcd615af
tonnitikorn1 <c.nitikorn@softkingdoms.com>
2023-09-08 15:48:48 +0700

    setpermission

--- services/serverService.js
+++ services/serverService.js
@@ -29,9 +29,9 @@ export const signIn = async (user) => {
     let resData = profile.data;
     localStorage.setItem("username", resData.username);
 
-    // const jsonData = JSON.stringify(resData.role_name.permission);
+    const jsonData = JSON.stringify(resData.role_name.permission);
 
-    // const encodedText = stringToHex(jsonData);
+    const encodedText = stringToHex(jsonData);
 
     // const text = 'testSet'
      localStorage.setItem("role", 'text');
